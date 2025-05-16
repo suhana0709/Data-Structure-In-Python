a68c98de119c96500ef495ad40fd86ef64aac725
suhana0709 <farjanajaman1975@gmail.com>
2025-05-16 07:39:20 -0500

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,19 @@
+#an empty list
+print("An empty list:-")
+empty = []
+print(empty)
+
+#a list with numbers
+print("A list with numbers:-")
+numbers = [1, 2, 3]
+print(numbers)
+
+#triple a list
+print("Tripling (x3) a list:-")
+triple = numbers*3
+print(triple)
+
+#reversing a list
+print("Reversing a list:-")
+reverse = numbers[::-1]
+print(reverse)
