3ce9bd940deb2dbe08787a84e90e201a75be5fb6
suhana0709 <farjanajaman1975@gmail.com>
2025-05-29 09:36:07 -0500

    Add files via upload

---
+++ set_symmetric_difference.py
@@ -0,0 +1,8 @@
+set1 = {1, 2, 3, 4, 5, 5}
+set2 = {1, 6, 9, 3}
+print("Original set:-")
+print(set1)
+print(set2)
+set3 = set1.symmetric_difference(set2)
+print("\n Symmetric Difference:-")
+print(set3)
